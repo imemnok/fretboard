ad535d34e2fb2ac361e83c8a91cda62c56389d41
Matt Halloran <matthew.j.halloran@avanade.com>
2018-10-03 12:19:10 -0700

    additional instruments added pre redux

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { Fretboard } from './fretboard.js'
 
 class App extends Component {
   render() {
+
     return (
       <div>
-      <Fretboard />
+      <div>
+        <h1 className="Title">Guitar Mode Visualizer</h1>
+      </div>
+      <div>
+        <Fretboard />
+      </div>
+      
       </div>
     );
   }
